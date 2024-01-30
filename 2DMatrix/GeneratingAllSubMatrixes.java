c3c291109d4309c0fdf5f09304665a7c8ae73755
Tharun <98168648+Tharun48@users.noreply.github.com>
2024-01-30 23:59:24 +0530

    Update GeneratingAllSubMatrixes.java

--- 2DMatrix/GeneratingAllSubMatrixes.java
+++ 2DMatrix/GeneratingAllSubMatrixes.java
@@ -1,19 +1,26 @@
 class Solution {
-    public void numSubmatrixSumTarget(int[][] matrix) {
+    private int getSum(int row,int col,int n,int m,int matrix[][]) {
+        int sum=0;
+        for(int i=row;i<=n;i++) {
+            for(int j=col;j<=m;j++) {
+                sum += matrix[i][j];
+            }
+        }
+        return sum;
+    }
+    public int numSubmatrixSumTarget(int[][] matrix, int target) {
         int n = matrix.length;
         int m = matrix[0].length;
+        int ans=0;
         for(int rowStart=0;rowStart<n;rowStart++) {
-            for(int rowSize=1;rowStart+rowSize<=n;rowSize++) {
+            for(int rowEnd=rowStart;rowEnd<n;rowEnd++) {
                 for(int colStart=0;colStart<m;colStart++) {
-                    for(int colSize=1;colStart+colSize<=m;colSize++) {
-                        System.out.println("rowStart = " + row + " colStart = " + col + " n = " + n + " m = " + m);
+                    for(int colEnd=colStart;colEnd<m;colEnd++) {
+                        if(getSum(rowStart,colStart,rowEnd,colEnd,matrix)==target) ans++;
                     }
                 }
             }
         }
+        return ans;
     }
-  public static void main(String args[]) {
-    int matrix[][]={{0,1,0},{1,1,1},{0,1,0}};
-    numSubmatrixSumTarget(matrix);
-  }
 }
