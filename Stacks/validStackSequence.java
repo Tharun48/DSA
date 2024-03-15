290cf38b4797333f7b58e7bf9a9ebc4e76140421
Tharun <98168648+Tharun48@users.noreply.github.com>
2024-03-15 09:25:58 +0530

    Create validStackSequence.java

---
+++ Stacks/validStackSequence.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        int n = pushed.length;
+        int i = 0;
+        int j = 0;
+        Stack<Integer> stack = new Stack();
+        while(i<n) {
+            if(stack.isEmpty() || stack.peek()!=popped[j]) {
+                stack.push(pushed[i]);
+                i++;
+            }
+            while(!stack.isEmpty() && stack.peek()==popped[j]) {
+                stack.pop();
+                j++;
+            } 
+        }
+        if(stack.isEmpty())
+            return true;
+        return false;
+    }
+}
+
+class Main {
+      public static void main(String args[]) {
+            Solution s = new Solution();
+            int pushed[] = {1,2,3,4,5};
+            int poped[] = {4,5,3,2,1};
+            System.out.println(s.validateStackSequences(pushed,poped));
+      }
+}
