3b0c7583fe6ed6da8a6c4209647a71188ad47376
Tharun <98168648+Tharun48@users.noreply.github.com>
2024-08-26 08:32:43 +0530

    Update Dijkstra.java

--- Graphs/Dijkstra.java
+++ Graphs/Dijkstra.java
@@ -1,38 +1,50 @@
-class Pair {
-    int distance;
+class Node{
     int node;
-    Pair(int distance,int node) {
+    int weight;
+    Node(int node,int weight) {
+        this.node=node;
+        this.weight=weight;
+    }
+}
+class Pair {
+    long distance;
+    int node;
+    Pair(long distance,int node) {
         this.distance = distance;
         this.node = node;
     }
 }
-
 class Solution
 {
-    static int[] dijkstra(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj, int S)
+    static long[] dijkstra(int V, List<List<Node>> adj, int S)
     {
-        int dist[] = new int[V];
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->a.distance-b.distance);
+        long dist[] = new long[V];
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> Long.compare(a.distance, b.distance));
         
         pq.offer(new Pair(0,S));
-        Arrays.fill(dist,Integer.MAX_VALUE);
+        Arrays.fill(dist,Long.MAX_VALUE);
         dist[S] =0;
         
         while(!pq.isEmpty()) {
             Pair p = pq.peek();
             int node = p.node;
-            int weight = p.distance;
+            long weight = p.distance;
+            
+            // System.out.println("node = " + node + " weigth = " + weight);
+            
+            
             pq.poll();
             int size = adj.get(node).size();
-           
+            
             for(int i=0;i<size;i++) {
-                int neighBournode = adj.get(node).get(i).get(0);
-                int neighBourWeight = adj.get(node).get(i).get(1);
-                if(dist[neighBournode] > weight + neighBourWeight ) {
+                int neighBournode = adj.get(node).get(i).node;
+                long neighBourWeight = adj.get(node).get(i).weight;
+                if(dist[neighBournode] > (long)weight + neighBourWeight ) {
                     dist[neighBournode] = weight + neighBourWeight;
                     pq.offer(new Pair(weight + neighBourWeight,neighBournode));
-                }
+                }   
             }
+            
         }
         return dist;
     }
